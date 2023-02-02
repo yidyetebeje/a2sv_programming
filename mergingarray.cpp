f4ed755dbdf05f6e411960204f6de4dc0931f74e
Yidnekachew Tebeje <93323621+yidyedelina@users.noreply.github.com>
2023-02-02 17:52:38 +0300

    added mergingarray.cpp

---
+++ mergingarray.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast                 \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+const ll mod = 1e9 + 7, N = 2e6 + 7, M = 2e6 + 7, INF = INT_MAX / 10;
+ll powe(ll x, ll y)
+{
+    x = x % mod, y = y % (mod - 1);
+    ll ans = 1;
+    while (y > 0)
+    {
+        if (y & 1)
+        {
+            ans = (1ll * x * ans) % mod;
+        }
+        y >>= 1;
+        x = (1ll * x * x) % mod;
+    }
+    return ans;
+}
+void solve()
+{
+}
+signed main()
+{
+    fast;
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cin >> b[i];
+    }
+    int i = 0, j = 0;
+    vector<int> c;
+
+    while (i < n && j < m)
+    {
+        if (a[i] < b[j])
+        {
+            c.push_back(a[i]);
+            i++;
+        }
+        else
+        {
+            c.push_back(b[j]);
+            j++;
+        }
+    }
+    while (i < n)
+    {
+        c.push_back(a[i]);
+        i++;
+    }
+    while (j < m)
+    {
+        c.push_back(b[j]);
+        j++;
+    }
+    for (int i = 0; i < c.size(); i++)
+    {
+        cout << c[i] << " ";
+    }
+    return 0;
+}
