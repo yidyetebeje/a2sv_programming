66e47198c1e40db3adc05ab9389451244a791cf6
Yidnekachew Tebeje <93323621+yidyedelina@users.noreply.github.com>
2023-02-24 17:57:08 +0300

    added restoresequence.cpp

---
+++ restoresequence.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast                 \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+const ll mod = 1e9 + 7, N = 2e6 + 7, M = 2e6 + 7, INF = INT_MAX / 10;
+ll powe(ll x, ll y)
+{
+    x = x % mod, y = y % (mod - 1);
+    ll ans = 1;
+    while (y > 0)
+    {
+        if (y & 1)
+        {
+            ans = (1ll * x * ans) % mod;
+        }
+        y >>= 1;
+        x = (1ll * x * x) % mod;
+    }
+    return ans;
+}
+void solve(vector<int>& v)
+{
+    int n = v.size();
+    vector<int> ans(n + 1);
+    int l = 0, r = n - 1;
+    while(l <= r){
+        if(l == r)
+            cout << v[l];
+       else  cout << v[l] << " " << v[r] << " ";
+        l++;
+        r--;
+    }
+    cout << endl;
+}
+signed main()
+{
+    fast;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        solve(v);
+    }
+    return 0;
+}
