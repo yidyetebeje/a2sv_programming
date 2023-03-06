26e1e4c9a80c2b5cfe629bbe150c87a55e6d8874
Yidnekachew Tebeje <93323621+yidyedelina@users.noreply.github.com>
2023-03-06 09:45:04 +0300

    added reverse-string.cpp

--- reverse-string.cpp
+++ reverse-string.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
+    void reverse(vector<char>& s, int l, int r){
+        if(l >= r) return;
+        swap(s[r], s[l]);
+        l++, r--;
+        reverse(s,l,r);
+    }
     void reverseString(vector<char>& s) {
-        reverse(s.begin(), s.end());
+        int len = s.size();
+        int l = 0, r = len - 1;
+       reverse(s,l,r);
     }
 };
