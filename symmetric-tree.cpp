1f16308247bc859e8be11d6224df6a52481835e1
Yidnekachew Tebeje <93323621+yidyedelina@users.noreply.github.com>
2023-03-09 09:27:53 +0300

    added symmetric-tree.cpp

---
+++ symmetric-tree.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool symmetric(TreeNode* left, TreeNode* right){
+        if(left == NULL && right == NULL) return true;
+        if(left == NULL || right == NULL) return false;
+        return (left->val == right->val) && symmetric(left->left, right->right) && symmetric(right->left, left->right);
+    }
+    bool isSymmetric(TreeNode* root) {
+       return symmetric(root->left, root->right);
+    }
+};
