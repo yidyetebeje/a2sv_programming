758ba4f447973275eb9045ad539bcccf094cc577
Yidnekachew Tebeje <93323621+yidyedelina@users.noreply.github.com>
2023-03-09 09:38:16 +0300

    added find-right-interval.cpp

---
+++ find-right-interval.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        vector<int> ans(intervals.size());
+        vector<pair<int,int>> mp;
+        map<int, int> m;
+        for(int i = 0; i < intervals.size(); i++)
+            m[intervals[i][0]] = i;
+        for(int i = 0; i < intervals.size(); i++) 
+               ans[i]  = m.lower_bound(intervals[i][1])!=end(m)?m.lower_bound(intervals[i][1]) -> second:-1;
+        return ans;
+    }
+};
