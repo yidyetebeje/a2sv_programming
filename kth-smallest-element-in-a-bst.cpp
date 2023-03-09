7aa5df74dac21c1c9da342f77cba39e56f4d8925
Yidnekachew Tebeje <93323621+yidyedelina@users.noreply.github.com>
2023-03-09 09:32:34 +0300

    added kth-smallest-element-in-a-bst.cpp

---
+++ kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> vi;
+    void inorder(TreeNode* root){
+        if(root == NULL) return;
+        inorder(root->left);
+        vi.push_back(root->val);
+        inorder(root->right);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        inorder(root);
+        return vi[k - 1];
+    }
+};
