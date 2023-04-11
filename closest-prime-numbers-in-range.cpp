3bfbbf1551cb43a99549993b30f1329b86740169
Yidnekachew Tebeje <93323621+yidyedelina@users.noreply.github.com>
2023-04-11 22:15:53 +0300

    added closest-prime-numbers-in-range.cpp

---
+++ closest-prime-numbers-in-range.cpp
@@ -0,0 +1,33 @@
+class Solution 
+{
+public:
+    vector<int> closestPrimes(int left, int right) 
+    {
+        vector<int> nums(right+1, 1);
+        for(int i=2; i*i<=right; i++)
+        {
+            if(nums[i]==1)
+            {
+                for(int j=i*i; j<=right; j+=i)
+                    nums[j]=0;
+            }
+        }
+        vector<int> prime;
+        for(int i=max(2,left); i<=right; i++)
+        {
+            if(nums[i]==1) prime.push_back(i);
+        }
+        int n1=-1, n2=-1, mini=1e8, diff;
+        for(int i=1; i<prime.size(); i++)
+        {
+            diff = prime[i]-prime[i-1];
+            if(diff < mini)
+            {
+                mini = diff;
+                n1 = prime[i-1];
+                n2 = prime[i];
+            }
+        }
+        return {n1, n2};
+    }
+};
