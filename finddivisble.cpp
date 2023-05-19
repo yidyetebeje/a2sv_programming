7373705d0aa3d28c27c014c6cbd92e1443a73a51
Yidnekachew Tebeje <93323621+yidyedelina@users.noreply.github.com>
2023-05-19 22:56:58 +0300

    added finddivisble.cpp

---
+++ finddivisble.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        int x,y;
+        cin >> x >> y;
+        cout << x << " " << 2*x << endl;
+    }
+}
