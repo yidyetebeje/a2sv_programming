479039106f4529114f75fc8f3754a285493cb6c1
Yidnekachew Tebeje <93323621+yidyedelina@users.noreply.github.com>
2023-05-25 19:07:00 +0300

    added increasing-triplet-subsequence.cpp

---
+++ increasing-triplet-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int min1 = INT_MAX;
+            int min2 = INT_MAX;
+
+        for (int num : nums) {
+            if (num <= min1) {
+                min1 = num;
+            } else if (num <= min2) {
+                min2 = num;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
