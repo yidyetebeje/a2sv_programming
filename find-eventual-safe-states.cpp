9867b779c8bd6fefbc981f76a30ec4e454f7fe53
Yidnekachew Tebeje <93323621+yidyetebeje@users.noreply.github.com>
2023-07-07 10:23:34 +0300

    added find-eventual-safe-states.cpp

--- find-eventual-safe-states.cpp
+++ find-eventual-safe-states.cpp
@@ -2,14 +2,37 @@ class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> colors(n, 0);
-        vector<int> result;
+        vector<int> inDegree(n, 0);
+        vector<vector<int>> adj(n);
         for (int i = 0; i < n; i++) {
-            if (dfs(i, graph, colors)) {
-                result.push_back(i);
+            for (auto node : graph[i]) {
+                adj[node].push_back(i);
+                inDegree[i]++;
             }
         }
-        return result;
+        for(auto x: inDegree){
+            cout << x << " ";
+        }
+        deque<int> q;
+        for(int i = 0; i < n; i++){
+            if(inDegree[i] == 0){
+                q.push_back(i);
+            }
+        }
+        vector<int> safe;
+        while(!q.empty()){
+            int top = q.front();
+            safe.push_back(top);
+            for(int i = 0; i < adj[top].size(); i++){
+                inDegree[adj[top][i]]--;
+                if(inDegree[adj[top][i]] == 0){
+                    q.push_back(adj[top][i]);
+                }
+            }
+            q.pop_front();
+        }
+        sort(safe.begin(),safe.end());
+        return safe;
     }
     bool dfs(int node, vector<vector<int>>& graph, vector<int>& colors) {
         if (colors[node] != 0) {
