0bfcf2be6260f153733fc85730c39d6c959a4dcf
Yidnekachew Tebeje <93323621+yidyetebeje@users.noreply.github.com>
2023-07-19 22:35:15 +0300

    added target-sum.cpp

--- target-sum.cpp
+++ target-sum.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
-    int dp[21][1001];
-    int solve(int n, int sum, vector<int>&a){
-        if(n==0){
-            if(sum==0){
-                return 1;
-            }
-            else{
-                return 0;
-            }
+    struct pair_hash {
+        template <typename T1, typename T2>
+        std::size_t operator()(const std::pair<T1, T2>& p) const {
+            auto hash1 = std::hash<T1>{}(p.first);
+            auto hash2 = std::hash<T2>{}(p.second);
+            return hash1 ^ hash2;
         }
-        if(sum<0){
-            return 0;
-        }
-        if(dp[n][sum]!=-1){
-            return dp[n][sum];
-        }
-        return dp[n][sum] = solve(n-1,sum-a[n-1],a) + solve(n-1,sum,a);
-    }
-    int findTargetSumWays(vector<int>& a, int target) {
-        
-        memset(dp,-1,sizeof(dp));
-        if(target<0){
-            target*=-1;
-        }
-        int sum = 0 ;
-        for(int &x:a){
-            sum+=x;
-        }
-        if((sum+target)%2==1){
-            return 0;
-        }
-        int req = (sum-target)/2;
-        return solve(a.size(),req,a);
-
-    }
+    };
+  int recursive(vector<int>& nums, int sum, int i, int target,unordered_map<pair<int,int>,int, pair_hash>& dp){
+      if(i == nums.size()){
+          if(sum == target){
+              return 1;
+          }
+          return 0;
+      }
+      if(dp.count({sum, i})) return dp[{sum, i}];
+      dp[{sum ,i}] = recursive(nums, sum + nums[i], i + 1, target,dp) + recursive(nums, sum - nums[i], i + 1, target,dp);
+      return dp[{sum , i}];
+  }
+  int findTargetSumWays(std::vector<int> nums, int S) {
+      unordered_map<pair<int,int>,int, pair_hash> dp;
+      return recursive(nums, 0, 0, S,dp);
+  }
 };
