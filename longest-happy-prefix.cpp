26a4d3024d999405df14a1d46df6af13ae6b9700
Yidnekachew Tebeje <93323621+yidyetebeje@users.noreply.github.com>
2023-10-10 19:12:19 +0300

    added longest-happy-prefix.cpp

---
+++ longest-happy-prefix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> prefix_function(string s) {
+        int n = (int)s.length();
+        vector<int> pi(n);
+        for (int i = 1; i < n; i++) {
+            int j = pi[i-1];
+            while (j > 0 && s[i] != s[j])
+                j = pi[j-1];
+            if (s[i] == s[j])
+                j++;
+            pi[i] = j;
+        }
+        return pi;
+    }
+    string longestPrefix(string s) {
+        vector<int> kmp = prefix_function(s);
+        int maxE = kmp[kmp.size() - 1];
+        return s.substr(0, maxE);
+    }
+};
